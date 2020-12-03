0df9bf393162a50cfc5026771e524aec16510f46
Anton Fagerberg <anton.fagerberg@husqvarnagroup.com>
2020-12-03 15:20:20 +0100

    Setup project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2020
+
+Trying to learn some Clojure this year.
---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject aoc2020 "0.1.0-SNAPSHOT"
+  :description "Advent of Code 2020 - learning some clojure"
+  :url "https://www.antonfagerberg.com"
+  :dependencies [[org.clojure/clojure "1.10.1"]])
